29bffcfd411e5d4350b1b8643eec172635bc1acd
Siva Prakash <prakashdeveloper@outlook.in>
2025-07-04 23:01:58 +0530

    Added constant values to token.go & Added go.mod file

--- token/token.go
+++ token/token.go
@@ -6,3 +6,20 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT" // add, foobar, x, y, ...
+	INT       = "INT"   // 1343456
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
