6baef8c0dbcbf6c0812b56281e61b6dfacd07e94
Siva Prakash <prakashdeveloper@outlook.in>
2025-07-09 23:15:22 +0530

    Added NextToken() for map character to token to lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,3 +27,28 @@ func (l *Lexer) readChar() {
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
